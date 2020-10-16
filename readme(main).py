5512680fd87cbf3dfb99c5d9bc3c34ea15c3e0e2
pycodermegax <69017203+pycodermegax@users.noreply.github.com>
2020-10-15 21:03:59 -0700

    Click Here
    Main

--- readme(main).py
+++ readme(main).py
@@ -1,1 +1,3 @@
-Hey guys, I have this python folder lying around so I thought it might come into use. So i made a exe if it, and so ya. Apche Lisence 2.0 (NO IDEA HOW TO SPELL IT!HAHA!)
+Hey guys, I have this python folder lying around so I thought it might come into use. So i made a exe if it, and so ya. Apche Lisence 2.0 (NO IDEA HOW TO SPELL IT!HAHA!) I call it Fort SuiteSEVar.
+ So got to this link to download it: https://drive.google.com/u/0/uc?id=1mxwam10Qzn2XZfuwUH1xMvX0YAAIdlne&export=download (it might take some time, and it is super safe)
+    
